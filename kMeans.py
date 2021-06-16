96df8cd9d6a21737492fbc49e7dd4c7ff71eef91
TKCODED <TahsinKazi@outlook.com>
2021-06-16 04:20:55 +0100

    Adds 3D visualization of data.

--- kMeans.py
+++ kMeans.py
@@ -37,9 +37,9 @@ def kMeans(NOofCentroids, data):
             index = np.nanargmin([np.linalg.norm(point - centroid) for centroid in
                                   centroids])  # Returns index of the centroid that is closest to point
             assignments[index].append(point)  # Assigns each point to a centroid
-        assignments = np.array(assignments)
+        assignments = np.array(assignments, np.float32)
         centroids = np.array([np.mean(assignments[index], axis=0) for index in range(
-            NOofCentroids)])  # Gets the mean point of each assignments and makes it the new centroid for that cluster
+            NOofCentroids)], np.float32)  # Gets the mean point of each assignments and makes it the new centroid for that cluster
         prevCentroids = temp
     assignments = assignments.tolist()
     if dimensions == 2:
@@ -274,6 +274,55 @@ data = [[5.1, 3.5, 1.4]
     , [5.7, 2.9, 4.2]
     , [6.2, 2.9, 4.3]
     , [5.1, 2.5, 3.0]
-    , [5.7, 2.8, 4.1]]
-
-kMeans(2, data)
+    , [5.7, 2.8, 4.1]
+    , [6.3, 3.3, 6.0]
+    , [5.8, 2.7, 5.1]
+    , [7.1, 3.0, 5.9]
+    , [6.3, 2.9, 5.6]
+    , [6.5, 3.0, 5.8]
+    , [7.6, 3.0, 6.6]
+    , [4.9, 2.5, 4.5]
+    , [7.3, 2.9, 6.3]
+    , [6.7, 2.5, 5.8]
+    , [7.2, 3.6, 6.1]
+    , [6.5, 3.2, 5.1]
+    , [6.4, 2.7, 5.3]
+    , [6.8, 3.0, 5.5]
+    , [5.7, 2.5, 5.0]
+    , [5.8, 2.8, 5.1]
+    , [6.4, 3.2, 5.3]
+    , [6.5, 3.0, 5.5]
+    , [7.7, 3.8, 6.7]
+    , [7.7, 2.6, 6.9]
+    , [6.0, 2.2, 5.0]
+    , [6.9, 3.2, 5.7]
+    , [5.6, 2.8, 4.9]
+    , [7.7, 2.8, 6.7]
+    , [6.3, 2.7, 4.9]
+    , [6.7, 3.3, 5.7]
+    , [7.2, 3.2, 6.0]
+    , [6.2, 2.8, 4.8]
+    , [6.1, 3.0, 4.9]
+    , [6.4, 2.8, 5.6]
+    , [7.2, 3.0, 5.8]
+    , [7.4, 2.8, 6.1]
+    , [7.9, 3.8, 6.4]
+    , [6.4, 2.8, 5.6]
+    , [6.3, 2.8, 5.1]
+    , [6.1, 2.6, 5.6]
+    , [7.7, 3.0, 6.1]
+    , [6.3, 3.4, 5.6]
+    , [6.4, 3.1, 5.5]
+    , [6.0, 3.0, 4.8]
+    , [6.9, 3.1, 5.4]
+    , [6.7, 3.1, 5.6]
+    , [6.9, 3.1, 5.1]
+    , [5.8, 2.7, 5.1]
+    , [6.8, 3.2, 5.9]
+    , [6.7, 3.3, 5.7]
+    , [6.7, 3.0, 5.2]
+    , [6.3, 2.5, 5.0]
+    , [6.5, 3.0, 5.2]
+    , [6.2, 3.4, 5.4]
+    , [5.9, 3.0, 5.1]]
+kMeans(3, data)
