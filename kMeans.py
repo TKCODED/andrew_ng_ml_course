998c2b20740c861810a658c8e95dce167603c9f7
TKCODED <TahsinKazi@outlook.com>
2021-06-16 03:57:57 +0100

    Adds 3D visualization of data.

--- kMeans.py
+++ kMeans.py
@@ -2,6 +2,7 @@ from random import randrange
 import numpy as np
 from time import perf_counter as pf
 from matplotlib import pyplot as plt
+from mpl_toolkits import mplot3d
 
 
 def timer(function):
@@ -26,7 +27,8 @@ def kMeans(NOofCentroids, data):
     assignments = [[] for _ in range(NOofCentroids)]
     for dim in range(1, dimensions):
         centroids = np.append(centroids, np.array(
-            [[randrange(np.min(np.ceil(data[:, dim])), np.max(np.ceil(data[:, dim]))) for _ in range(NOofCentroids)]]).transpose(),
+            [[randrange(np.min(np.ceil(data[:, dim])), np.max(np.ceil(data[:, dim]))) for _ in
+              range(NOofCentroids)]]).transpose(),
                               1)  # Initializes centroid with random values in the range of the data
     while not np.array_equal(centroids, prevCentroids):  # Loops until centroids do not change after an iteration
         temp = centroids
@@ -40,12 +42,13 @@ def kMeans(NOofCentroids, data):
             NOofCentroids)])  # Gets the mean point of each assignments and makes it the new centroid for that cluster
         prevCentroids = temp
     assignments = assignments.tolist()
-    for i in range(len(assignments)):
-        if type(assignments[i]) == list:
-            for j in range(len(assignments[i])):
-                assignments[i][j] = assignments[i][j].tolist()
-        else:
-            assignments[i] = assignments[i].tolist()
+    if dimensions == 2:
+        for i in range(len(assignments)):
+            if type(assignments[i]) == list:
+                for j in range(len(assignments[i])):
+                    assignments[i][j] = assignments[i][j].tolist()
+            else:
+                assignments[i] = assignments[i].tolist()
     if dimensions == 2:
         fig = plt.figure()
         ax = fig.gca()
@@ -53,9 +56,22 @@ def kMeans(NOofCentroids, data):
         for centroid in centroids:
             ax.scatter(centroid[0], centroid[1])
         plt.show()
-    print(f'Centroids = {centroids.tolist()}; Assignments = {assignments}')
+    if dimensions == 3:
+        fig = plt.figure()
+        ax = plt.axes(projection='3d')
+        ax.scatter3D(data[:, 0], data[:, 1], data[:, 2])
+        for centroid in centroids:
+            ax.scatter(centroid[0], centroid[1], centroid[2])
+        plt.show()
+
+    string = f'Centroids = {centroids.tolist()}; Assignments ='
+    for assignment in assignments:
+        string += f'{assignment}////////////////////////////'
+    print(string)
     return centroids.tolist(), assignments
 
+
+# Anderson Iris Set; Features included: Sepal Length and Width, Petal Length
 data = [[4.9, 3.0]
     , [4.7, 3.2]
     , [4.6, 3.1]
@@ -155,4 +171,106 @@ data = [[4.9, 3.0]
     , [6.2, 2.9]
     , [5.1, 2.5]]
 
+'''
+data = [[5.1, 3.5, 1.4]
+    , [4.9, 3.0, 1.4]
+    , [4.7, 3.2, 1.3]
+    , [4.6, 3.1, 1.5]
+    , [5.0, 3.6, 1.4]
+    , [5.4, 3.9, 1.7]
+    , [4.6, 3.4, 1.4]
+    , [5.0, 3.4, 1.5]
+    , [4.4, 2.9, 1.4]
+    , [4.9, 3.1, 1.5]
+    , [5.4, 3.7, 1.5]
+    , [4.8, 3.4, 1.6]
+    , [4.8, 3.0, 1.4]
+    , [4.3, 3.0, 1.1]
+    , [5.8, 4.0, 1.2]
+    , [5.7, 4.4, 1.5]
+    , [5.4, 3.9, 1.3]
+    , [5.1, 3.5, 1.4]
+    , [5.7, 3.8, 1.7]
+    , [5.1, 3.8, 1.5]
+    , [5.4, 3.4, 1.7]
+    , [5.1, 3.7, 1.5]
+    , [4.6, 3.6, 1.0]
+    , [5.1, 3.3, 1.7]
+    , [4.8, 3.4, 1.9]
+    , [5.0, 3.0, 1.6]
+    , [5.0, 3.4, 1.6]
+    , [5.2, 3.5, 1.5]
+    , [5.2, 3.4, 1.4]
+    , [4.7, 3.2, 1.6]
+    , [4.8, 3.1, 1.6]
+    , [5.4, 3.4, 1.5]
+    , [5.2, 4.1, 1.5]
+    , [5.5, 4.2, 1.4]
+    , [4.9, 3.1, 1.5]
+    , [5.0, 3.2, 1.2]
+    , [5.5, 3.5, 1.3]
+    , [4.9, 3.6, 1.4]
+    , [4.4, 3.0, 1.3]
+    , [5.1, 3.4, 1.5]
+    , [5.0, 3.5, 1.3]
+    , [4.5, 2.3, 1.3]
+    , [4.4, 3.2, 1.3]
+    , [5.0, 3.5, 1.6]
+    , [5.1, 3.8, 1.9]
+    , [4.8, 3.0, 1.4]
+    , [5.1, 3.8, 1.6]
+    , [4.6, 3.2, 1.4]
+    , [5.3, 3.7, 1.5]
+    , [5.0, 3.3, 1.4]
+    , [7.0, 3.2, 4.7]
+    , [6.4, 3.2, 4.5]
+    , [6.9, 3.1, 4.9]
+    , [5.5, 2.3, 4.0]
+    , [6.5, 2.8, 4.6]
+    , [5.7, 2.8, 4.5]
+    , [6.3, 3.3, 4.7]
+    , [4.9, 2.4, 3.3]
+    , [6.6, 2.9, 4.6]
+    , [5.2, 2.7, 3.9]
+    , [5.0, 2.0, 3.5]
+    , [5.9, 3.0, 4.2]
+    , [6.0, 2.2, 4.0]
+    , [6.1, 2.9, 4.7]
+    , [5.6, 2.9, 3.6]
+    , [6.7, 3.1, 4.4]
+    , [5.6, 3.0, 4.5]
+    , [5.8, 2.7, 4.1]
+    , [6.2, 2.2, 4.5]
+    , [5.6, 2.5, 3.9]
+    , [5.9, 3.2, 4.8]
+    , [6.1, 2.8, 4.0]
+    , [6.3, 2.5, 4.9]
+    , [6.1, 2.8, 4.7]
+    , [6.4, 2.9, 4.3]
+    , [6.6, 3.0, 4.4]
+    , [6.8, 2.8, 4.8]
+    , [6.7, 3.0, 5.0]
+    , [6.0, 2.9, 4.5]
+    , [5.7, 2.6, 3.5]
+    , [5.5, 2.4, 3.8]
+    , [5.5, 2.4, 3.7]
+    , [5.8, 2.7, 3.9]
+    , [6.0, 2.7, 5.1]
+    , [5.4, 3.0, 4.5]
+    , [6.0, 3.4, 4.5]
+    , [6.7, 3.1, 4.7]
+    , [6.3, 2.3, 4.4]
+    , [5.6, 3.0, 4.1]
+    , [5.5, 2.5, 4.0]
+    , [5.5, 2.6, 4.4]
+    , [6.1, 3.0, 4.6]
+    , [5.8, 2.6, 4.0]
+    , [5.0, 2.3, 3.3]
+    , [5.6, 2.7, 4.2]
+    , [5.7, 3.0, 4.2]
+    , [5.7, 2.9, 4.2]
+    , [6.2, 2.9, 4.3]
+    , [5.1, 2.5, 3.0]
+    , [5.7, 2.8, 4.1]]
+'''
 kMeans(2, data)
