16554af0d00d6e59afe1403542bce67b7cee3429
TKCODED <TahsinKazi@outlook.com>
2021-06-25 21:12:20 +0100

    Neural Network Builder.

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -1,8 +1,8 @@
 import numpy as np
-import random
 from time import perf_counter as pf
+import random
 from matplotlib import pyplot as plt
-from mpl_toolkits import mplot3d
+import pandas as pd
 
 
 def timer(function):
@@ -13,7 +13,6 @@ def timer(function):
         time = t1 - t0
         print(function.__name__, "took", time, "second.")
         return proccessedFunc
-
     return wrapper
 
 
@@ -21,7 +20,8 @@ class network:
     def __init__(self, config, inputs, labels, lamda=0):
         self.config = config  # List of the number of units for each layer excluding the input layer and including the output layer
         self.input = self.inputLayer(inputs)
-        self.layers = [self.layer(config[i]) for i in range(len(config))]
+        self.layers = [self.layer(config[i + 1], config[i]) for i in range(len(config) - 1)]
+        self.layers.insert(0, self.layer(config[0], len(inputs[0]) - 1))
         self.labels = labels
         self.y = np.zeros((len(inputs), self.config[-1]))
         for index in range(len(
@@ -31,71 +31,40 @@ class network:
         self.lamda = lamda
         self.cost = 0
 
-    def forward(self, input):  # Takes in a datapoint at a time
-        self.layers[0].activation = [self.sigmoid(np.dot(self.layers[0].weights[k, :], input)) for k in
-                                     range(len(self.layers[0].weights))]
-        self.layers[0].z = [np.dot(self.layers[0].weights[k, :], input) for k in
-                            range(len(self.layers[0].weights))]
-        self.layers[0].activation.insert(0, 1)
-        self.layers[0].activation = np.array(self.layers[0].activation)
-        for j in range(1, len(self.config) - 1):
-            self.layers[j].activation = [
-                self.sigmoid(np.dot(self.layers[j].weights[k, :], self.layers[j - 1].activation)) for k in
-                range(len(self.layers[j].weights))]
-            self.layers[j].activation.insert(0, 1)
-            self.layers[j].activation = np.array(self.layers[j].activation)
-        self.layers[-1].activation = [
-            self.sigmoid(np.dot(self.layers[-1].weights[k, :], self.layers[-2].activation))
-            for k in range(len(self.layers[-1].weights))]
-        tempArr = np.array(self.layers[-1].activation).reshape(1, self.config[-1])
-        self.results = np.vstack((self.results, tempArr[0]))
-        self.results = np.round(self.results, 10)
+    def train(self, inputs):# Works only for balanced networks and regularization for gradients have not been added
+        #Forward
+        self.layers[0].z = np.matmul(inputs, self.layers[0].weights.transpose())
+        self.layers[0].activation = self.sigmoid(self.layers[0].z)
+        self.layers[0].activation = np.insert(self.layers[0].activation, 0, 1, axis=1)
+        for l in range(1, len(self.layers)):
+            self.layers[l].z= np.matmul(self.layers[l-1].activation, self.layers[l].weights.transpose())
+            self.layers[l].activation = self.sigmoid(self.layers[l].z)
+            if l != len(self.layers) - 1:
+                self.layers[l].activation = np.insert(self.layers[l].activation, 0, 1, axis=1)
+        self.results = np.vstack((self.results, self.layers[-1].activation))
+       #Backprop
+        d = []
+        d.append(np.subtract(self.layers[-1].activation, self.y))#Does d for the output layer
+        for l in range(len(self.layers) - 1):
+            epsilon = np.multiply(np.matmul(d[-1], self.layers[-2 - l].weights[:, 1:]), self.sigmoidGradient(self.layers[-2 - l].z))
+            d.append(epsilon)
+        d = d[::-1]
+        deltas =[]
+        deltas.append(np.matmul(d[0].transpose(), inputs))# Does delta for inputs
+        for l in range(1, len(self.layers)):
+            deltas.append(np.matmul(self.layers[len(self.layers) - 1 - l].activation.transpose(), d[l]).transpose()/len(inputs))
+        for j in range(len(self.layers)):
+            self.layers[j].weights = np.add(self.layers[j].weights, deltas[j])
+        print("COST:", self.findCost())
 
     def findCost(self):  # Finds cost after the entire dataset has been proccessed
+        self.results = np.round(self.results, 10)
         self.results[self.results == 1] = 0.999999999
         self.results[self.results == 0] = 0.000000001
         self.cost = -np.sum(np.multiply(self.y[0:len(self.results), :], np.log(self.results)) - np.multiply(
-            np.subtract(1, self.y[0:len(self.results), :]), np.log(self.results))) / len(self.results) + (
-                            self.lamda / (2 * len(self.results))) * np.sum(
-            np.sum([self.layers[j].weights for j in range(len(self.layers))]))
+            np.subtract(1, self.y[0:len(self.results), :]), np.log(self.results))) / len(self.results) \
+                    #+ (self.lamda / (2 * len(self.results))) * np.sum(np.sum([self.layers[j].weights for j in range(len(self.layers))]))
         return self.cost
-
-    def backward(self, input):  # Takes in a datapoint at a time
-        dOut = np.subtract(self.y[len(self.results) - 1, :], self.layers[
-            -1].activation)  # It uses the length of results to get the index of the input to get the correct y-value
-        dOut = dOut.reshape(-1, 1)
-        d = []
-        d.append(dOut)
-        deltas = []
-        for j in range(len(self.layers) - 1):
-            # print(d[j].shape, j)
-            # print(self.layers[-2 - j].activation.reshape(-1,1).transpose().shape, j)
-            delta = np.matmul(d[j], self.layers[-2 - j].activation.reshape(-1, 1).transpose())
-            deltas.append(delta)
-            sigmoidGradient = self.sigmoidGradient(np.array(self.layers[-2 - j].z)).reshape(-1, 1)
-            d.append(np.multiply(np.matmul(self.layers[-2 - j].weights[:, 1:], d[-1]),
-                                 sigmoidGradient))  # SIGMOID GRADIENT INDEX MIGHT BE WRONG
-        delta = np.multiply(d[-1], input.reshape(-1, 1).transpose())
-        deltas.append(delta)
-        # d.append(np.multiply(d[-1], self.layers[-2 - j].weights[:, 1:-1])) d1 not needed as we are not going to change the inputs of the network
-        deltas = deltas[::-1]
-        weightsGradients = deltas
-        return weightsGradients
-        # for j in range(1, len(self.layers)):
-        #     np.add(self.layers[j].weights, weigthsGradients[j - 1])
-        # TODO: number of weight gradients wrong the indexing is wrong
-        # TODO: Weights Gradients divide by the length of self.results which does not make sense since the backprop algorithm only does it for one input
-
-    def batch(self):
-        self.forward(self.input.inputs[0])
-        weightsGradients = self.backward(self.input.inputs[0])
-        for i in range(1, len(self.input.inputs)):
-            print("--------------------------")
-            self.forward(self.input.inputs[i])
-            weightsGradients = np.add(weightsGradients, self.backward(self.input.inputs[i]))
-        for j in range(len(self.layers)):
-            self.layers[j].weights = np.add(self.layers[j].weights, weightsGradients[j])
-        print("COST:", self.findCost())
 
     def sigmoid(self, input):
         return 1 / (1 + np.exp(-input))
@@ -104,9 +73,10 @@ class network:
         return np.multiply(self.sigmoid(input), (1 - self.sigmoid(input)))
 
     class layer:
-        def __init__(self, units):
+        def __init__(self, units, prevNOunits):
             self.units = units
-            self.weights = np.array([[random.randint(0, 30) for _ in range(units + 1)] for _ in range(units)])
+            self.prevNOunits = prevNOunits
+            self.weights = np.array([[random.randint(0, 30) for _ in range(self.units + 1)] for _ in range(self.prevNOunits)])
             self.activation = []
             self.z = []
 
@@ -124,8 +94,7 @@ class network:
             data /= np.std(data, axis=0)
             return data
 
-
-n = network([2, 2], [[3, 5], [4, 3]], [1, 2])
+n = network([3, 3], [[3, 5, 3], [4, 3, 7]], [3, 1])
 print("WEIGHTS:", [n.layers[j].weights for j in range(len(n.layers))])
-n.batch()
+n.train(n.input.inputs)
 print("WEIGHTS:", [n.layers[j].weights for j in range(len(n.layers))])
