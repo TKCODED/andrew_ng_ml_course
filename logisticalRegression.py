8c439d2be8737b3fdbab60ec7c67840d17bb89b1
TKCODED <TahsinKazi@outlook.com>
2021-06-28 01:35:09 +0100

    Logistical Regression.

--- logisticalRegression.py
+++ logisticalRegression.py
@@ -13,22 +13,22 @@ def timer(function):
         time = t1 - t0
         print(function.__name__, "took", time, "second.")
         return proccessedFunc
+
     return wrapper
 
 
 def precproccessing(data):  # So infinities do not come from the const function
     data = np.subtract(data, np.mean(data, axis=0))
     data /= np.std(data, axis=0)
-    data[:,0] = 1
+    data[:, 0] = 1
     return data
 
 
 def logisticalReg(inputs, outputs, RATE=1):
-    print(inputs)
     NOofFeatures = len(inputs[0])
     NOofItems = len(inputs)
-    mean = np.mean(np.array(inputs), axis=0)[1]#Gets the mean of the variabele
-    std = np.std(np.array(inputs)[:,1])#Gets the standard deviation of the variabele
+    mean = np.mean(np.array(inputs), axis=0)[1]  # Gets the mean of the variabele
+    std = np.std(np.array(inputs)[:, 1])  # Gets the standard deviation of the variabele
     inputs = precproccessing(inputs)
     outputs = np.array(outputs, np.float64).reshape(len(outputs), 1)
     weights = np.array([random.randint(1, 3) for _ in range(NOofFeatures)], np.float64).reshape(NOofFeatures, 1)
@@ -38,17 +38,19 @@ def logisticalReg(inputs, outputs, RATE=1):
     iters = []
     costs = []
 
-    while index < 5000:
+    while index < 50000:
         tempCost = cost
         temp = weights
         weights = np.subtract(weights, ((RATE / NOofItems) * np.matmul(inputs.transpose(), np.array(
-            [(1 / (1 + np.exp(-np.dot(temp.transpose(), inputs[i,:]))) - outputs[i]) for i in range(NOofItems)]))))
-        activationOfInputs = [1 / (1 + np.exp(-np.dot(weights.transpose(), inputs[i,:]))) for i in range(NOofItems)]
+            [(1 / (1 + np.exp(-np.dot(temp.transpose(), inputs[i, :]))) - outputs[i]) for i in range(NOofItems)]))))
+        activationOfInputs = [1 / (1 + np.exp(-np.dot(weights.transpose(), inputs[i, :]))) for i in range(NOofItems)]
         activationOfInputs = np.array([i.tolist() for i in activationOfInputs])
         activationOfInputs[activationOfInputs == 1] = 0.9999999
         activationOfInputs[activationOfInputs == 0] = 0.0000001
-        cost = np.nansum(np.subtract(np.multiply(outputs, -np.log(activationOfInputs)),   np.multiply(np.subtract(1, outputs),
-                                                 np.log(np.subtract(1, activationOfInputs))))) / NOofItems
+        cost = np.nansum(
+            np.subtract(np.multiply(outputs, -np.log(activationOfInputs)), np.multiply(np.subtract(1, outputs),
+                                                                                       np.log(np.subtract(1,
+                                                                                                          activationOfInputs))))) / NOofItems
         costs.append(cost)
         index += 1
         iters.append(index)
@@ -62,8 +64,26 @@ def logisticalReg(inputs, outputs, RATE=1):
     print(f'Mean = {mean}, Standard Deviation = {std}')
     print(f"Function is: 1/1+e^-({string})")  # Prints Logistical Regression equation
     print("Cost is:", cost)
-    #plt.plot(iters, costs)
-    #plt.show()
+    x = np.linspace(0, len(inputs), 1000)
+    y = np.zeros(1000)
+    expression = [weights[i] * (((x - mean) / std) ** i) for i in range(len(weights))]
+    for term in expression:
+        y = np.add(y, term)
+    plt.xlim([0, len(outputs) + 1])
+    plt.ylim([-10, 10])
+    plt.grid()
+    plt.title("Decision Boundary")
+    points = []
+    for point in range(len(outputs)):
+        points.append(point + 1) if outputs[point] == 1 else None
+    plt.scatter(points, [0 for _ in range(len(points))])
+    points = []
+    for point in range(len(outputs)):
+        points.append(point + 1) if outputs[point] == 0 else None
+    plt.scatter(points, [0 for _ in range(len(points))], marker='^')
+    plt.plot(x, y, 'r')
+    # plt.plot(iters, costs)
+    plt.show()
     return (weights, cost)
 
 
@@ -78,8 +98,9 @@ def learningCurve(inputs, outputs, rate=1, size=None):
     plt.plot(iters, costs)
     plt.show()
 
-inputs = [[1, i,i**2,i**3,i**4,i**5,i**6] for i in range(1,10)]
-outputs = [1, 1, 1, 0, 0, 0, 0, 1, 1]
+
+outputs = [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0]
+inputs = [[1, i, i**2, i**3] for i in range(1, len(outputs) + 1)]
 # df = pd.read_csv('bottle.csv')
 # input = df['Salnty'].to_numpy()
 # inputs = np.array([np.power(input,i) for i in range(3)]).transpose()#Power to the 0 means the x1 column is included
@@ -92,6 +113,6 @@ outputs = [1, 1, 1, 0, 0, 0, 0, 1, 1]
 # outputs[outputs <= 15] = 0
 # outputs[outputs > 15] = 1
 
-print(learningCurve(inputs, outputs))
+print(logisticalReg(inputs, outputs))
 
 # print(logisticalReg(inputs, outputs))
